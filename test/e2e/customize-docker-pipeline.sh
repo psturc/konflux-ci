81a9a309131da4aab79f5549cff2fc45917fa80c
Pavel Sturc <psturc@redhat.com>
2024-11-05 14:30:22 +0100

    fix: replace computeResources requests

--- test/e2e/customize-docker-pipeline.sh
+++ test/e2e/customize-docker-pipeline.sh
@@ -24,8 +24,9 @@ main() {
     yq e -i 'del(.spec.tasks[] | select(.name == "clair-scan"))' $tmp_pipeline_path
     # Workaround 2
     # CPU and memory requests are too high for the build-container Task's "build" step to run in GitHub actions
-    # Thus this workaround removes ".computeResources.requests" from the "build" step and updates the build Pipeline that is used in CI to reference the updated Task
-    yq e -i "del(.spec.steps[] | select(.name == \"build\").computeResources.requests)" $tmp_build_task_path
+    # Thus this workaround lowers ".computeResources.requests" from the "build" step and updates the build Pipeline that is used in CI to reference the updated Task
+    yq e -i "(.spec.steps[].computeResources.requests.cpu) |= \"100m\"" $tmp_build_task_path
+    yq e -i "(.spec.steps[].computeResources.requests.memory) |= \"256Mi\"" $tmp_build_task_path
     yq e -i "(.spec.tasks[] | select(.name == \"build-container\").taskRef.params[] | select(.name == \"bundle\").value) |= \"${CUSTOMIZED_BUILD_TASK_IMAGE_REF_CLUSTER}\"" $tmp_pipeline_path
 
     # Push the modified Pipeline and Task bundles to the local container registry
